3a2cb1b32bbc450fc15f2c72e9046154a6ee206e
neusc <neusc829@163.com>
2018-03-29 23:20:18 +0800

    递归调用函数visit获取html中的所有a标签的href链接

---
+++ func/findlinks1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+// 获取html中a便签元素节点的href属性，并返回一个slice集合
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
