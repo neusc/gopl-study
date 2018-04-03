081c7b89d0cb7d188f44959b10abb0fa85681340
neusc <neusc829@163.com>
2018-04-03 22:11:00 +0800

    fix:html节点遍历方式

--- func/outline2/main.go
+++ func/outline2/main.go
@@ -33,8 +33,8 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
 		pre(n)
 	}
-	for c := n.FirstChild; c != nil; c = n.NextSibling {
-		forEachNode(n, pre, post)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
 	}
 	if post != nil {
 		post(n)
@@ -52,6 +52,7 @@ func startElement(n *html.Node) {
 
 func endElement(n *html.Node) {
 	if n.Type == html.ElementNode {
+		depth--
 		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
 	}
 }
