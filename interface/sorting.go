f279abafe048de75bcfe24b4f3929dde280569ac
neusc <neusc829@163.com>
2018-04-29 19:44:24 +0800

    自定义类型实现sort.Interface接口，实现对列表按不同字段排序

---
+++ interface/sorting.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"time"
+	"text/tabwriter"
+	"os"
+	"fmt"
+	"sort"
+)
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+// 使用text/tabwriter包来生成一个列是整齐对齐和隔开的表格
+// *tabwriter.Writer是满足io.Writer借口，它会收集每一片写向它的数据；
+// 它的Flush方法会格式化整个表格并且将它写向os.Stdout（标准输出）
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "------", "------", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush()
+}
+
+// 定义一个实现sort.Interface接口的类型
+// 实现sort.Interface接口需要以下三个方法
+
+// 按照Artist字段排序
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+// 按照Year字段排序
+type byYear []*Track
+
+func (x byYear) Len() int           { return len(x) }
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+func main() {
+	fmt.Println("\nbyArtist:")
+	sort.Sort(byArtist(tracks))
+	printTracks(tracks)
+	sort.Sort(sort.Reverse(byArtist(tracks))) // 按照Artist字段逆序排序，不需要定义一个有颠倒Less方法的新类型byReverseArtist
+	printTracks(tracks)
+
+	fmt.Println("\nbyYear")
+	sort.Sort(byYear(tracks))
+	printTracks(tracks)
+}
+
+
