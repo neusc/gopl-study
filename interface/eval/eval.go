5150e31c9f44f390fb9eed9d81b051b0ec57e6e6
neusc <neusc829@163.com>
2018-05-01 14:41:29 +0800

    表达式求值器中类型的定义以及每种类型对应的eval方法实现

---
+++ interface/eval/eval.go
@@ -0,0 +1,78 @@
+package eval
+
+import (
+	"fmt"
+	"math"
+)
+
+type Expr interface {
+	Eval(env Env) float64 //在environment变量中计算表达式的值
+}
+
+type Var string
+
+type literal float64
+
+type unary struct {
+	op rune
+	x  Expr
+}
+
+type binary struct {
+	op   rune
+	x, y Expr
+}
+
+type call struct {
+	fn   string
+	args []Expr
+}
+
+// 使用environment变量将变量的名字映射成对应的值
+type Env map[Var]float64
+
+// 每个表达式定义一个Eval方法
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+
+func (l literal) Eval(_ Env) float64 {
+	return float64(l)
+}
+
+// 递归计算运算对象
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", b.op))
+}
+
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+	}
+	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
+}
