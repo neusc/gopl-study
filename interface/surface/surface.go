214728edd40c0ee2615c838d2eddf17df1cc97aa
neusc <neusc829@163.com>
2018-05-04 10:32:49 +0800

    net/http包提供了一个全局的ServeMux实例DefaultServerMux和包级别的http.Handle和http.HandleFunc函数。现在，为了使用DefaultServeMux作为服务器的主handler，我们不需要将它传给ListenAndServe函数；nil值就可以工作

--- interface/surface/surface.go
+++ interface/surface/surface.go
@@ -106,6 +106,10 @@ func plot(w http.ResponseWriter, r *http.Request) {
 
 //!+main
 func main() {
+	/**
+	net/http包提供了一个全局的ServeMux实例DefaultServerMux和包级别的http.Handle和http.HandleFunc函数。
+	现在，为了使用DefaultServeMux作为服务器的主handler，我们不需要将它传给ListenAndServe函数；nil值就可以工作
+	 */
 	http.HandleFunc("/plot", plot)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
