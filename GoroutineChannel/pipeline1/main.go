42900ce1ab0be3137f4b30f1f86a02e5e83bc12f
neusc <neusc829@163.com>
2018-05-08 17:46:04 +0800

    串联channels的改进版本，通过channel获取数据的第二个参数知道channel是否已经被关闭

--- GoroutineChannel/pipeline1/main.go
+++ GoroutineChannel/pipeline1/main.go
@@ -8,21 +8,26 @@ func main() {
 
 	// 计数器
 	go func() {
-		for x := 0; ; x++ {
+		for x := 0; x < 100; x++ {
 			naturals <- x
 		}
+		close(naturals) // 适当条件关闭channel，不再向其中发送数据
 	}()
 
 	// 接收naturals channel传来的整数并求平方，将结果传入squares channel
 	go func() {
 		for {
-			x := <-naturals
+			x, ok := <-naturals
+			if !ok {
+				break // 退出循环当无法从channel获取到值，即channel已经关闭
+			}
 			squares <- x * x
 		}
+		close(squares)
 	}()
 
 	// 打印从squares channel接收到的数据
-	for {
-		fmt.Println(<-squares)
+	for x := range squares {
+		fmt.Println(x)
 	}
 }
