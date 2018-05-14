ab3d41a7a595e9777af96a97efe164419b59dfbb
neusc <neusc829@163.com>
2018-05-14 14:27:46 +0800

    基于goroutine以及通过channel实现广播信息，实现的聊天服务器

---
+++ GoroutineChannel/chat/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"net"
+	"bufio"
+	"fmt"
+	"log"
+)
+
+type client chan<- string
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string)
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // 所有已连接到服务器的客户端
+	for {
+		select {
+		case msg := <-messages:
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+// 每个连接到服务器的客户端单独启用一个goroutine用于处理连接相关操作
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String() // 根据客户端的ip地址区别用户
+	// 新建连接的相关操作
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+
+	// 连接断开的相关操作
+	leaving <- ch
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
