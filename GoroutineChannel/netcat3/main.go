40af6b51ef296a6b7521ff5d9da885c6fd5c9a84
neusc <neusc829@163.com>
2018-05-14 14:59:42 +0800

    客户端关闭标准输入之后mustCopy函数返回，主goroutine继续往下执行

--- GoroutineChannel/netcat3/main.go
+++ GoroutineChannel/netcat3/main.go
@@ -19,7 +19,7 @@ func main() {
 		log.Println("done")
 		done <- struct{}{} // 后台goroutine完成后向主goroutine发送完成信号
 	}()                         // 另一个goroutine从连接中读取信息并打印服务器的响应
-	mustCopy(conn, os.Stdin)    // main goroutine从标准输入流中读取内容并发送到服务器
+	mustCopy(conn, os.Stdin)    // main goroutine从标准输入流中读取内容并发送到服务器，关闭标准输入之后此函数返回继续往下执行
 	conn.Close()
 	<-done // 主goroutine接收到后台goroutine完成信号后退出程序
 }
