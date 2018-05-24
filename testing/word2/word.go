1ba301c4f2536115e4b1128369cb576442020233
neusc <neusc829@163.com>
2018-05-24 15:22:34 +0800

    过滤掉字符串中非字母字符之后，再次测试通过

---
+++ testing/word2/word.go
@@ -0,0 +1,20 @@
+package word
+
+import "unicode"
+
+// 判断一个字符串是否从前往后和从后往前读是一致的
+// 忽略非字母字符
+func IsPalindrome(s string) bool {
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ testing/word2/word_test.go
@@ -0,0 +1,31 @@
+package word
+
+import "testing"
+
+// 参数t用于报告测试失败和附加的日志信息
+func TestIsPalindrome(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false}, // non-palindrome
+		{"desserts", false},   // semi-palindrome
+	}
+
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf("IsPalindrome(%q) = %v", test.input, got)
+		}
+	}
+}
